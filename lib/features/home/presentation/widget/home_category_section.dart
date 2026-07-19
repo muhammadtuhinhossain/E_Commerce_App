52a99700ceb6ff3c525132168567749dc46e3bd0
MohammadTuhin1 <muhammadtuhin.dev@gmail.com>
2026-07-19 20:26:47 +0600

    implement category list on home screen

--- lib/features/home/presentation/widget/home_category_section.dart
+++ lib/features/home/presentation/widget/home_category_section.dart
@@ -1,4 +1,6 @@
+import 'package:crafty_bay/features/category/presentation/provider/category_list_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../app/app_colors.dart';
 import '../../../shared/presentation/widget/category_card.dart';
@@ -10,13 +12,25 @@ class HomeCategorySection extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 110,
-      child: ListView.separated(
-        scrollDirection: .horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          //return CategoryCard();
-        },
-        separatorBuilder: (_, _) => SizedBox(width: 8),
+      child: Consumer<CategoryListProvider>(
+        builder: (context, categoryListProvider, _) {
+          if(categoryListProvider.isInitialLoading){
+            return SizedBox(
+              height: 110,
+              child: Center(child: CircularProgressIndicator()),
+            );
+          }
+          return ListView.separated(
+            scrollDirection: .horizontal,
+            itemCount: categoryListProvider.categoryList.length > 10 
+            ?10
+            :categoryListProvider.categoryList.length,
+            itemBuilder: (context, index) {
+              return CategoryCard(categoryModel: categoryListProvider.categoryList[index],);
+            },
+            separatorBuilder: (_, _) => SizedBox(width: 8),
+          );
+        }
       ),
     );
   }
